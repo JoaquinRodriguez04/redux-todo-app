c44b534361621002869e0e7c7c7d9ad26b0db7a0
Joaquin <JoaquindeveloperWeb@gmail.com>
2023-08-28 21:06:32 -0300

    se agrego el App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import TodosList from './components/todosList';
+import TodosForm from './components/TodosForm';
+import TodoBackAnimated from './components/TodoBackAnimated';
+import TodoBanner from './components/todoBanner';
+import TodoHero from './components/TodoHero';
+import TodoNavBar from './components/TodoNavBar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className='app-container'>
+      <TodoBackAnimated/>
+      <TodoBanner/>
+      <TodoNavBar/>
+      <TodoHero/>
+      <TodosList/>
+      <TodosForm/>
+    </main>
   )
-}
+};
 
-export default App
+export default App;
